958677c48190e7733737e98b1e3f1aa3330bcc8a
Sean Liu <sean@precision.localdomain>
2020-09-03 22:50:34 -0400

    Add argument parser, remove the cmd code from DataModel.py

--- python3/DataModel.py
+++ python3/DataModel.py
@@ -467,71 +467,11 @@ class ImageProcessing(object):
             return
         logging.debug("saving image...")
         tiff.imsave(full_path, img)
-        tmp_exif = Tyf.open(full_path)
-        tmp_exif.load_raster()
-        if exif and isinstance(exif, Tyf.TiffFile):
-            logging.debug("saving exif...")
-            exif[0].stripes = tmp_exif[0].stripes
-            #exif.save(full_path)
-
-if __name__ == "__main__":
-    logging_level = logging.DEBUG
-    logging_format = "%(asctime)s (%(name)s) [%(levelname)s] line %(lineno)d: %(message)s"
-    logging.basicConfig(format=logging_format, level=logging_level)
-    #logging.basicConfig(format=logging_format, level=logging_level, filename="logs.txt")
-    
-    data_model = DataModel()
-    #img_tmpl = u"/Volumes/ZJJ-4TB/Photos/17.08.21 Eclipse Trip/6DII/IMG_{:04d}_0.tif"
-    img_tmpl = u"DSC028{:2d}.tif"
-    for p in [img_tmpl.format(i) for i in (26,23,24,25,27,28,29,30)]:
-        logging.debug("image: %s", p)
-        data_model.add_image(p)
-    
-    ref_img = data_model.images[0]
-    #f = ref_img.focal_len
-    f=16.5
-    img_shape = ref_img.fullsize_gray_image.shape
-    img_size = np.array([img_shape[1], img_shape[0]])
-    data_model.reset_result()
-
-    pts, vol = ImageProcessing.detect_star_points(ref_img.fullsize_gray_image)
-    sph = ImageProcessing.convert_to_spherical_coord(pts, np.array((img_shape[1], img_shape[0])), f)
-    feature = ImageProcessing.extract_point_features(sph, vol)
-    ref_img.features["pts"] = pts
-    ref_img.features["sph"] = sph
-    ref_img.features["vol"] = vol
-    ref_img.features["feature"] = feature
-
-    data_model.final_sky_img = np.copy(ref_img.original_image).astype("float32") / np.iinfo(
-        ref_img.original_image.dtype).max
-    # Initial sum image
-    sky_imgs=[np.copy(data_model.final_sky_img)]
-    #serial=0
-    #result_img = (data_model.final_sky_img * np.iinfo("uint16").max).astype("uint16")
-    #ImageProcessing.save_tif_image("test00.tif", result_img, data_model.images[0].exif_info)
-
-    for img in data_model.images[1:]:
-        pts, vol = ImageProcessing.detect_star_points(img.fullsize_gray_image)
-        sph = ImageProcessing.convert_to_spherical_coord(pts, img_size, f)
-        feature = ImageProcessing.extract_point_features(sph, vol)
-        img.features["pts"] = pts
-        img.features["sph"] = sph
-        img.features["vol"] = vol
-        img.features["feature"] = feature
-
-        pair_idx = ImageProcessing.find_initial_match(img.features, ref_img.features)
-        tf, pair_idx = ImageProcessing.fine_tune_transform(img.features, ref_img.features, pair_idx)
-        img_tf = cv2.warpPerspective(img.original_image, tf[0], tuple(img_size))
-        img_tf = img_tf.astype("float32") / np.iinfo(img_tf.dtype).max
-        #serial=serial+1
-        sky_imgs.append(np.copy(img_tf))
-        #result_img = (img_tf * np.iinfo("uint16").max).astype("uint16")
-        #ImageProcessing.save_tif_image("test{:02d}.tif".format(serial), result_img, data_model.images[0].exif_info)
-
-
-    #data_model.final_sky_img = sum_img/(serial+1)
-    data_model.final_sky_img = np.mean(np.asarray(sky_imgs),axis=0)
-
-    result_img = (data_model.final_sky_img * np.iinfo("uint16").max).astype("uint16")
-    ImageProcessing.save_tif_image("test.tif", result_img, data_model.images[0].exif_info)
+        # Not saving exif as it's problematic
+        #tmp_exif = Tyf.open(full_path)
+        #tmp_exif.load_raster()
+        #if exif and isinstance(exif, Tyf.TiffFile):
+        #   logging.debug("saving exif...")
+        #    exif[0].stripes = tmp_exif[0].stripes
+        #    exif.save(full_path)
 
